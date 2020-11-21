e8ab48f2f70c22ce13be322e646a2988759ecbbb
Mateusz Burzyński <mateuszburzynski@gmail.com>
2020-11-21 10:39:47 +0100

    Demo without treeshakeable reexport

---
+++ dist/parcel/index.a449c302.js
@@ -0,0 +1,2 @@
+!function(){var o,s=Object.prototype.hasOwnProperty;console.log((o={css:""},void(s.call(o,"css")||console.log("has css prop"))))}();
+//# sourceMappingURL=index.a449c302.js.map
--- dist/parcel/index.f7c7b3c5.js
+++
@@ -1,2 +0,0 @@
-console.log(void({}.call({css:""},"css")||console.log("has css prop")));
-//# sourceMappingURL=index.f7c7b3c5.js.map
